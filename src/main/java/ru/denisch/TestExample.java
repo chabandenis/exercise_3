eaf92effbcfbc72fa72a7b4eb21fcbc5b6c836c1
chabandenis <denisbrd8@gmail.com>
2019-09-01 18:23:01 +0700

    прошу проверить

--- src/main/java/ru/denisch/TestExample.java
+++ src/main/java/ru/denisch/TestExample.java
@@ -20,59 +20,59 @@ public class TestExample {
 
     @Before
     public void startBefore1() throws Exception {
-        System.out.println("startBefore1");
+        System.out.println("        startBefore1");
         throw new Exception();
         //return;
     }
 
     @Before
     public void startBefore2() {
-        System.out.println("startBefore2");
+        System.out.println("        startBefore2");
 
         return;
     }
 
     @Before
     public void startBefore3() {
-        System.out.println("startBefore3");
+        System.out.println("        startBefore3");
         return;
     }
 
 
     @After
     public void startAfter1() {
-        System.out.println("after void1");
+        System.out.println("        after void1");
         return;
     }
 
     @After
     public void startAfter2() throws Exception {
-        System.out.println("after void2");
+        System.out.println("        after void2");
         throw new Exception();
     }
 
     @After
     public void startAfter3() throws Exception {
-        System.out.println("after void3");
+        System.out.println("        after void3");
         throw new Exception();
     }
 
 
     @Test
     public void startTest1() {
-        System.out.println("test void1");
+        System.out.println("    test void1");
         return;
     }
 
     @Test
     public void startTest2() {
-        System.out.println("test void2");
+        System.out.println("        test void2");
         return;
     }
 
     @Test
     public void startTest3() throws Exception {
-        System.out.println("test void3");
+        System.out.println("        test void3");
         throw new Exception();
     }
 
--- src/main/java/ru/denisch/TestExe.java
+++ src/main/java/ru/denisch/TestExe.java
@@ -3,82 +3,96 @@ package ru.denisch;// запуск теста
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TestExe {
     // всего до
-    private static int allCntBefore;
+    private int allCntBefore;
     // тестов
-    private static int allCntTest;
+    private int allCntTest;
     // всего тестов после
-    private static int allCntAfter;
+    private int allCntAfter;
 
     // ошибок до
-    private static int errCntBefore;
+    private int errCntBefore;
     // ошибок тестов
-    private static int errCntTest;
+    private int errCntTest;
     // ошибок после
-    private static int errCntAfter;
+    private int errCntAfter;
 
 
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+
+        Set<Method> setBefor = new HashSet<>();
+        Set<Method> setTest = new HashSet<>();
+        Set<Method> setAfter = new HashSet<>();
+
+        TestExe testExe = new TestExe();
+
         Class cl = Class.forName(args[0]);
-//        System.out.println(cl);
 
         Method[] method = cl.getMethods();
 
-        Object beforTestObj = cl.newInstance();
-        Object testObj = cl.newInstance();
-        Object afterTestObj = cl.newInstance();
-
-        // test
-
         for (Method md : method) {
             if (md.isAnnotationPresent(Before.class)) {
-                allCntBefore++;
-                System.out.println("ru.denisch.Before " + md);
-                try {
-                    md.invoke(beforTestObj);
-                }
-                catch (Exception e){
-                    errCntBefore++;
-                }
+                setBefor.add(md);
+            } else if (md.isAnnotationPresent(Test.class)) {
+                setTest.add(md);
+            } else if (md.isAnnotationPresent(After.class)) {
+                setAfter.add(md);
             }
         }
 
-        for (Method md : method) {
-            if (md.isAnnotationPresent(Test.class)) {
-                allCntTest++;
-                System.out.println("ru.denisch.Test " + md);
+        System.out.println("методов с аннотацией @Before " + setBefor.size());
+        System.out.println("методов с аннотацией @After " + setAfter.size());
+        System.out.println("методов с аннотацией @Test " + setTest.size());
+
+        for (Method test : setTest) {
+            System.out.println("Проверяем метод " + test.getName() + "; " + test.getParameterCount());
+
+            Object object = cl.getDeclaredConstructor().newInstance();
+
+            System.out.println("    ru.denisch.Before ");
+            for (Method before : setBefor) {
+                testExe.allCntBefore++;
                 try {
-                    md.invoke(testObj);
-                }
-                catch (Exception e){
-                    errCntTest++;
+                    before.invoke(object);
+                } catch (Exception e) {
+                    testExe.errCntBefore++;
                 }
             }
-        }
 
-        for (Method md : method) {
-            if (md.isAnnotationPresent(After.class)) {
-                allCntAfter++;
-                System.out.println("ru.denisch.After " + md);
-                try {
-                    md.invoke(afterTestObj);
-                }
-                catch (Exception e){
-                    errCntAfter++;
-                }
-
+            testExe.allCntTest++;
+            System.out.println("    ru.denisch.Test " + test);
+            try {
+                test.invoke(object);
+            } catch (Exception e) {
+                testExe.errCntTest++;
             }
+
+
+            System.out.println("    ru.denisch.After ");
+            for (Method after : setAfter) {
+                testExe.allCntAfter++;
+                try {
+                    after.invoke(object);
+                } catch (Exception e) {
+                    testExe.errCntAfter++;
+                }
+            }
+
         }
 
         System.out.println("Статистика по запускам");
 
-        System.out.println("Всего тестов до " + allCntBefore + "; успешно " + (allCntBefore - errCntBefore) + "; ошибок " + errCntBefore);
+        System.out.println("Всего тестов до " + testExe.allCntBefore + "; успешно " + (testExe.allCntBefore - testExe.errCntBefore) + "; ошибок " + testExe.errCntBefore);
         // тестов
-        System.out.println("Тестов " + allCntTest + "; успешо " + (allCntTest - errCntTest) + "; ошибок " + errCntTest);
+        System.out.println("Тестов " + testExe.allCntTest + "; успешо " + (testExe.allCntTest - testExe.errCntTest) + "; ошибок " + testExe.errCntTest);
         // всего тестов после
-        System.out.println("Всего тестов после " + allCntAfter + "; успешо " + (allCntAfter - errCntAfter) + "; ошибок " + errCntAfter);
+        System.out.println("Всего тестов после " + testExe.allCntAfter + "; успешо " + (testExe.allCntAfter - testExe.errCntAfter) + "; ошибок " + testExe.errCntAfter);
 
     }
 }
